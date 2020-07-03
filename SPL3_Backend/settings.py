7e818cd10f0b44bbd378601cb0f51ba8a4912e2c
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-07-03 14:23:14 +0600

    added app for ML works

--- SPL3_Backend/settings.py
+++ SPL3_Backend/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODELS = os.path.join(BASE_DIR, 'duplicate_detector/models')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'duplicate_detector',
+    'api',
+
 ]
 
 MIDDLEWARE = [
---
+++ duplicate_detector/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class DuplicateDetectorConfig(AppConfig):
+
+
+
+    name = 'duplicate_detector'
