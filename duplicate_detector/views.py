d4ca91ebe9553c8ab909f3eb250a6dc1eb013ecb
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-07-13 14:36:12 +0600

    parsed mismatched ast methods from two files

--- duplicate_detector/views.py
+++ duplicate_detector/views.py
@@ -1,10 +1,11 @@
+import javalang
 from django.http import JsonResponse
 import json
 import requests
 import base64
 
 from .apps import DuplicateDetectorConfig
-
+from duplicate_detector.predictor.method_clone_detection import makeFinalOutputDecision
 
 def test_api(request):
     return JsonResponse({'res': DuplicateDetectorConfig.encoder_encoding_dim})
@@ -42,13 +43,45 @@ def check_prduplicates(request):
         )
 
 
+def parse_changed_methods(file1_content, file2_content):
+    file1_astmethods, file2_astmethods = [], []
+
+    try:
+        file1_tree, file2_tree = javalang.parse.parse(file1_content), javalang.parse.parse(file2_content)
+
+        for t in file1_tree.types[0].body:
+            if type(t) == javalang.tree.MethodDeclaration:
+                file1_astmethods.append(t)
+        for t in file2_tree.types[0].body:
+            if type(t) == javalang.tree.MethodDeclaration:
+                file2_astmethods.append(t)
+
+        return list(set(file1_astmethods) ^ set(file2_astmethods)), list(set(file2_astmethods) ^ set(file1_astmethods))
+    except Exception as e:
+        print(file1_content)
+        print(file2_content)
+
+        return None
+
+
 def check_for_duplicate(sample_changed_files_list):
     changed_files_intersections_contents_urls = []
 
     for pr1_changed_file in sample_changed_files_list[0]:
         for pr2_changed_file in sample_changed_files_list[1]:
-            if pr1_changed_file.contents_url.split('?')[0] == pr2_changed_file.contents_url.split('?')[0]:
+            if pr1_changed_file.contents_url.split('?')[0].endswith('.java') and (pr1_changed_file.contents_url.split('?')[0] == pr2_changed_file.contents_url.split('?')[0]):
                 changed_files_intersections_contents_urls.append([pr1_changed_file.contents_url, pr2_changed_file.contents_url])
 
     for changed_files_contents_urls in changed_files_intersections_contents_urls:
-        file_contents = [requests.get(content_url) for content_url in changed_files_contents_urls]
+        file_contents = [base64.b64decode(requests.get(content_url)) for content_url in changed_files_contents_urls]
+
+        disjoint_changed_astmethods_list = parse_changed_methods(file_contents[0], file_contents[1])
+
+        for file1_astmethod in disjoint_changed_astmethods_list[0]:
+            for file2_astmethod in disjoint_changed_astmethods_list[1]:
+                output = makeFinalOutputDecision(file1_astmethod, file2_astmethod)
+                print(output)
+
+
+
+    return False
