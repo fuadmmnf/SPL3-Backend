606cda94f69261fefb28c75c99ff11ad4e6782d0
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-08-03 10:59:42 +0600

    file repr by maxpooling methods

--- duplicate_detector/predictor/method_clone_detection.py
+++ duplicate_detector/predictor/method_clone_detection.py
@@ -40,216 +40,207 @@ nmt_encoder_model = load_model(abs_nmt_model_path)
 
 # print(nmt_encoder_model.summary())
 # quit()
+class MethodRepresentationCalculator:
 
-def maxpooling(arr):
-    stTreesVecs = np.asarray(arr)
-    maxpooledEncoding = np.amax(stTreesVecs, axis=0)
+    def maxpooling(self, arr):
+        stTreesVecs = np.asarray(arr)
+        maxpooledEncoding = np.amax(stTreesVecs, axis=0)
 
-    return maxpooledEncoding[0]
+        return maxpooledEncoding[0]
 
+    def __makeNmtRepresentation(self, maxpooled_stTrees):
+        nmt_encoded_stTrees = []
+        methodInput = []
+        for maxpooled_stTree in maxpooled_stTrees:
+            methodInput.extend(maxpooled_stTree.tolist())
 
-def makeNmtRepresentation(maxpooled_stTrees):
-    nmt_encoded_stTrees = []
-    methodInput = []
-    for maxpooled_stTree in maxpooled_stTrees:
-        methodInput.extend(maxpooled_stTree.tolist())
+        spaceForPadding = (max_timestamps * encoder_encoding_dim) - len(methodInput)
+        # print('method vector length actual: ', len(methodInput))
+        methodInput.extend([0 for x in range(spaceForPadding)])
 
-    spaceForPadding = (max_timestamps * encoder_encoding_dim) - len(methodInput)
-    # print('method vector length actual: ', len(methodInput))
-    methodInput.extend([0 for x in range(spaceForPadding)])
+        stTreeInput = np.asarray(methodInput).reshape(max_timestamps, encoder_encoding_dim)
+        input = np.asarray([stTreeInput])
 
-    stTreeInput = np.asarray(methodInput).reshape(max_timestamps, encoder_encoding_dim)
-    input = np.asarray([stTreeInput])
+        stTreeNmtRepr = nmt_encoder_model.predict(input, verbose=0)
+        nmt_encoded_stTrees.append(stTreeNmtRepr[0])
 
-    stTreeNmtRepr = nmt_encoder_model.predict(input, verbose=0)
-    nmt_encoded_stTrees.append(stTreeNmtRepr[0])
+        return self.maxpooling(nmt_encoded_stTrees)
 
-    return maxpooling(nmt_encoded_stTrees)
+    def __computeEncodedTree(self, node):
+        global StTreeNodesForMaxPooling
+        nodeVecs = {}
+        child_sum = np.zeros(encoder_encoding_dim)
+        childNodesList = []
 
+        for _, subnode in node:
 
-def computeEncodedTree(node):
-    global StTreeNodesForMaxPooling
-    nodeVecs = {}
-    child_sum = np.zeros(encoder_encoding_dim)
-    childNodesList = []
+            if subnode is None:
+                print('none')
 
-    for _, subnode in node:
+            subnode_str = str(subnode).replace('\"', '_').replace('\'', '_').replace(' ', '')
 
-        if subnode is None:
-            print('none')
-
-        subnode_str = str(subnode).replace('\"', '_').replace('\'', '_').replace(' ', '')
-
-        childNodesList.append({subnode_str: subnode.children})
-        fasttext_embedding = word_vectors[subnode_str].reshape(1, 128)
-        # print(fasttext_embedding.shape)
-        # quit()
-        subnode_encoding = encoder_model.predict(fasttext_embedding, verbose=0)
-        nodeVecs[subnode_str] = subnode_encoding
-
-        for child in subnode.children:
-            child_str = str(child).replace('\"', '_').replace('\'', '_').replace(' ', '')
-            fasttext_embedding = word_vectors[child_str].reshape(1, 128)
+            childNodesList.append({subnode_str: subnode.children})
+            fasttext_embedding = word_vectors[subnode_str].reshape(1, 128)
+            # print(fasttext_embedding.shape)
+            # quit()
             subnode_encoding = encoder_model.predict(fasttext_embedding, verbose=0)
-            nodeVecs[child_str] = subnode_encoding
+            nodeVecs[subnode_str] = subnode_encoding
 
-    for nodeDict in reversed(childNodesList):
-        for key in nodeDict:
-            for childNode in nodeDict[key]:
-                childNode_str = str(childNode).replace('\"', '_').replace('\'', '_').replace(' ', '')
-                nodeVecs[key] = nodeVecs[key] + nodeVecs[childNode_str]
+            for child in subnode.children:
+                child_str = str(child).replace('\"', '_').replace('\'', '_').replace(' ', '')
+                fasttext_embedding = word_vectors[child_str].reshape(1, 128)
+                subnode_encoding = encoder_model.predict(fasttext_embedding, verbose=0)
+                nodeVecs[child_str] = subnode_encoding
 
-    # eqn2 of paper
-    # node_str = str(node).replace('\"', '_').replace('\'', '_').replace(' ', '')
-    # fasttext_embedding = word_vectors[node_str]
-    # node_encoding = model.predict(fasttext_embedding, verbose=0)
-    # node_vec = node_encoding + child_sum
+        for nodeDict in reversed(childNodesList):
+            for key in nodeDict:
+                for childNode in nodeDict[key]:
+                    childNode_str = str(childNode).replace('\"', '_').replace('\'', '_').replace(' ', '')
+                    nodeVecs[key] = nodeVecs[key] + nodeVecs[childNode_str]
 
-    for key in nodeVecs:
-        StTreeNodesForMaxPooling.append(nodeVecs[key])
+        # eqn2 of paper
+        # node_str = str(node).replace('\"', '_').replace('\'', '_').replace(' ', '')
+        # fasttext_embedding = word_vectors[node_str]
+        # node_encoding = model.predict(fasttext_embedding, verbose=0)
+        # node_vec = node_encoding + child_sum
 
+        for key in nodeVecs:
+            StTreeNodesForMaxPooling.append(nodeVecs[key])
 
-def encodeStTrees():
-    global StatementTrees
-    global StTreeNodesForMaxPooling
-    maxpooled_stTrees = []
+    def __encodeStTrees(self):
+        global StatementTrees
+        global StTreeNodesForMaxPooling
+        maxpooled_stTrees = []
 
-    print('started encoding...')
+        print('started encoding...')
 
-    for tree in StatementTrees:
-        computeEncodedTree(tree)
+        for tree in StatementTrees:
+            self.__computeEncodedTree(tree)
 
-        maxpooled_stTrees.append(maxpooling(StTreeNodesForMaxPooling))
+            maxpooled_stTrees.append(self.maxpooling(StTreeNodesForMaxPooling))
 
-        # maxpooling and write all st tree of methods to file
-        StTreeNodesForMaxPooling = []
+            # maxpooling and write all st tree of methods to file
+            StTreeNodesForMaxPooling = []
 
-    print('finished encoding...')
+        print('finished encoding...')
 
-    return maxpooled_stTrees
+        return maxpooled_stTrees
 
+    def __clean_statement(self, statement):
+        for _, node in statement:
+            if hasattr(node, 'member'):
+                node.member = None
+            if hasattr(node, 'value'):
+                node.value = None
 
-def clean_statement(statement):
-    for _, node in statement:
-        if hasattr(node, 'member'):
-            node.member = None
-        if hasattr(node, 'value'):
-            node.value = None
+            if hasattr(node, 'documentation'):
+                node.documentation = None
+            if type(node) is not javalang.tree.ReferenceType:
+                if hasattr(node, 'name'):
+                    node.name = None
 
-        if hasattr(node, 'documentation'):
-            node.documentation = None
-        if type(node) is not javalang.tree.ReferenceType:
-            if hasattr(node, 'name'):
-                node.name = None
+    def __handleSubStatement(self, statement):
+        global StatementTrees
+        isStatementValid = True
+        subStatement = None
 
+        if hasattr(statement, 'body') and statement.body is not None:
+            subStatement = statement.body
+            statement.body = None
+        elif hasattr(statement, 'block') and statement.block is not None:
+            subStatement = statement.block
+            statement.block = None
+        elif hasattr(statement, 'statements'):
+            isStatementValid = False
+            subStatement = statement.statements
+            statement.statements = None
+        # elif hasattr(statement, 'expression'):
+        # 	subStatement = statement.expression
+        # 	del statement.expression
 
-def handleSubStatement(statement):
-    global StatementTrees
-    isStatementValid = True
-    subStatement = None
+        if isStatementValid:
+            StatementTrees.append(statement)
+        # appendToCorpus(statement)
 
-    if hasattr(statement, 'body') and statement.body is not None:
-        subStatement = statement.body
-        statement.body = None
-    elif hasattr(statement, 'block') and statement.block is not None:
-        subStatement = statement.block
-        statement.block = None
-    elif hasattr(statement, 'statements'):
-        isStatementValid = False
-        subStatement = statement.statements
-        statement.statements = None
-    # elif hasattr(statement, 'expression'):
-    # 	subStatement = statement.expression
-    # 	del statement.expression
+        if subStatement is not None:
+            self.__pluckStatementAndAppendToStatementTree(subStatement)
 
-    if isStatementValid:
-        StatementTrees.append(statement)
-    # appendToCorpus(statement)
+    def __pluckStatementAndAppendToStatementTree(self, statementBlock):
+        if type(statementBlock) is list:
+            for statement in statementBlock:
+                self.__handleSubStatement(statement)
+        else:
+            self.__handleSubStatement(statementBlock)
 
-    if subStatement is not None:
-        pluckStatementAndAppendToStatementTree(subStatement)
+    def __computeMethodRepresentation(self, astmethod):
+        global StatementTrees
+        try:
 
+            self.__clean_statement(astmethod)
+            # StatementTrees.append(t)
+            # appendToCorpus(t)
+            self.__pluckStatementAndAppendToStatementTree(astmethod)
+            encoded_stTrees = self.__encodeStTrees()
+            method_representation = self.__makeNmtRepresentation(encoded_stTrees)
+            # print(method_representation)
+            # print('\n\n\n')
 
-def pluckStatementAndAppendToStatementTree(statementBlock):
-    if type(statementBlock) is list:
-        for statement in statementBlock:
-            handleSubStatement(statement)
-    else:
-        handleSubStatement(statementBlock)
+            StatementTrees = []
 
+            return method_representation
 
-def computeMethodRepresentation(astmethod):
-    global StatementTrees
-    try:
+        except Exception as e:
+            print(e)
+            print('\n\n\n')
+            quit()
+            StatementTrees = []
+            return
 
-        clean_statement(astmethod)
-        # StatementTrees.append(t)
-        # appendToCorpus(t)
-        pluckStatementAndAppendToStatementTree(astmethod)
-        encoded_stTrees = encodeStTrees()
-        method_representation = makeNmtRepresentation(encoded_stTrees)
-        # print(method_representation)
-        # print('\n\n\n')
+    def __parseMethodAndReturnRepresentation(self, filepath, startline, endline):
+        # print(filepath)
+        global StatementTrees
+        try:
+            fileData = ''
+            with open(filepath, 'r', encoding='utf-8') as file:
+                fileData = file.readlines()
+            fileData = "\n".join(line for line in fileData[startline - 1: endline])
 
-        StatementTrees = []
+            code_segment = 'class Temp {\n' + fileData + '}'
+            # print(code_segment)
+            # quit()
+            tree = javalang.parse.parse(code_segment)  # Create your   here.
+            for t in tree.types[0].body:
+                if type(t) == javalang.tree.MethodDeclaration:
+                    self.self.__clean_statement(t)
+                    # StatementTrees.append(t)
+                    # appendToCorpus(t)
+                    self.__pluckStatementAndAppendToStatementTree(t)
+                    # print('starting calc')
+                    encoded_stTrees = self.__encodeStTrees()
+                    method_representation = self.__makeNmtRepresentation(encoded_stTrees)
+                    # print('finish calc')
 
-        return method_representation
+                    # print(method_representation)
+                    # print('\n\n\n')
 
-    except Exception as e:
-        print(e)
-        print('\n\n\n')
-        quit()
-        StatementTrees = []
-        return
+                    StatementTrees = []
 
+                    return method_representation
 
-def parseMethodAndReturnRepresentation(filepath, startline, endline):
-    # print(filepath)
-    global StatementTrees
-    try:
-        fileData = ''
-        with open(filepath, 'r', encoding='utf-8') as file:
-            fileData = file.readlines()
-        fileData = "\n".join(line for line in fileData[startline - 1: endline])
+        except Exception as e:
+            print('error file:' + filepath)
+            print(e)
+            print('\n\n\n')
+            # quit()
+            StatementTrees = []
+            return None
 
-        code_segment = 'class Temp {\n' + fileData + '}'
-        # print(code_segment)
-        # quit()
-        tree = javalang.parse.parse(code_segment)  # Create your   here.
-        for t in tree.types[0].body:
-            if type(t) == javalang.tree.MethodDeclaration:
-                clean_statement(t)
-                # StatementTrees.append(t)
-                # appendToCorpus(t)
-                pluckStatementAndAppendToStatementTree(t)
-                # print('starting calc')
-                encoded_stTrees = encodeStTrees()
-                method_representation = makeNmtRepresentation(encoded_stTrees)
-                # print('finish calc')
+    def calculateMethodRepresentation(self, astmethod):
+        method_repr = self.__computeMethodRepresentation(astmethod)
+        return method_repr
+        # methodDiff = np.absolute(method1Repr - method2Repr).reshape(1, 200)
 
-                # print(method_representation)
-                # print('\n\n\n')
-
-                StatementTrees = []
-
-                return method_representation
-
-    except Exception as e:
-        print('error file:' + filepath)
-        print(e)
-        print('\n\n\n')
-        # quit()
-        StatementTrees = []
-        return None
-
-
-def makeFinalOutputDecision(astmethod1, astmethod2):
-    method1Repr = computeMethodRepresentation(astmethod1)
-    method2Repr = computeMethodRepresentation(astmethod2)
-
-    methodDiff = np.absolute(method1Repr - method2Repr).reshape(1, 200)
-
-    clone_probability = DuplicateDetectorConfig.astnn_model.predict(methodDiff)
-    print('prediction: ' + str(clone_probability[0][0]))
-
-    return clone_probability[0][0]
+        # clone_probability = DuplicateDetectorConfig.astnn_model.predict(methodDiff)
+        # print('prediction: ' + str(clone_probability[0][0]))
+        #
+        # return clone_probability[0][0]
--- duplicate_detector/views.py
+++ duplicate_detector/views.py
@@ -1,11 +1,8 @@
-import javalang
 from django.http import JsonResponse
 import json
-import requests
-import base64
 
 from .apps import DuplicateDetectorConfig
-from duplicate_detector.predictor.method_clone_detection import makeFinalOutputDecision
+from .evaluator.prduplicate_evaluator import PrduplicateDetector
 
 def test_api(request):
     return JsonResponse({'res': DuplicateDetectorConfig.encoder_encoding_dim})
@@ -19,69 +16,5 @@ def check_prduplicates(request):
         return JsonResponse({'res': 'must be post request with pr list data'})
 
     data = json.loads(request.body)
-    pr_list = data['pr_list']
+    PrduplicateDetector().detect_duplicate(data['pr_list'])
 
-    pr_changed_files_list = []
-
-    for pr in pr_list:
-        changed_files = requests.get(pr.url + '/files')
-        pr_changed_files_list.append({
-            'pr_id': pr.id,
-            'changed_files': changed_files
-        })
-
-    pr_dublicate_detection_scramble = []
-    for i in range(len(pr_list)):
-        for j in range(i, len(pr_list)):
-            pr_dublicate_detection_scramble.append([pr_list[i], pr_list[j]])
-
-
-    for sample in pr_dublicate_detection_scramble:
-        is_duplicate = check_for_duplicate([
-            changed_files for changed_files in pr_changed_files_list
-            if changed_files['pr_id'] == sample[0].id or changed_files['pr_id'] == sample[1].id]
-        )
-
-
-def parse_changed_methods(file1_content, file2_content):
-    file1_astmethods, file2_astmethods = [], []
-
-    try:
-        file1_tree, file2_tree = javalang.parse.parse(file1_content), javalang.parse.parse(file2_content)
-
-        for t in file1_tree.types[0].body:
-            if type(t) == javalang.tree.MethodDeclaration:
-                file1_astmethods.append(t)
-        for t in file2_tree.types[0].body:
-            if type(t) == javalang.tree.MethodDeclaration:
-                file2_astmethods.append(t)
-
-        return list(set(file1_astmethods) ^ set(file2_astmethods)), list(set(file2_astmethods) ^ set(file1_astmethods))
-    except Exception as e:
-        print(file1_content)
-        print(file2_content)
-
-        return None
-
-
-def check_for_duplicate(sample_changed_files_list):
-    changed_files_intersections_contents_urls = []
-
-    for pr1_changed_file in sample_changed_files_list[0]:
-        for pr2_changed_file in sample_changed_files_list[1]:
-            if pr1_changed_file.contents_url.split('?')[0].endswith('.java') and (pr1_changed_file.contents_url.split('?')[0] == pr2_changed_file.contents_url.split('?')[0]):
-                changed_files_intersections_contents_urls.append([pr1_changed_file.contents_url, pr2_changed_file.contents_url])
-
-    for changed_files_contents_urls in changed_files_intersections_contents_urls:
-        file_contents = [base64.b64decode(requests.get(content_url)) for content_url in changed_files_contents_urls]
-
-        disjoint_changed_astmethods_list = parse_changed_methods(file_contents[0], file_contents[1])
-
-        for file1_astmethod in disjoint_changed_astmethods_list[0]:
-            for file2_astmethod in disjoint_changed_astmethods_list[1]:
-                output = makeFinalOutputDecision(file1_astmethod, file2_astmethod)
-                print(output)
-
-
-
-    return False
